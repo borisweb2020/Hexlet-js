b6e2032dcd2bff5280414efc18485e4bebd36120
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2022-06-27 15:04:48 +0700

    install _lodash

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Hello, Hexlet!');
+
+const fs = 'A new string ....';
+console.log(fs);
+
+const _ = require("lodash");
+let p = _.intersection([2, 1], [2, 3]);
+let g = _.last(['one', 'two']);
+console.log(p, g);
